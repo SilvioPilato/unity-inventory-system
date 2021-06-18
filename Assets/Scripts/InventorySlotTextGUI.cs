8d55310f63d79cec492e1700e98aaee0093cee74
Silvio Pilato <pilatosil@gmail.com>
2021-06-18 12:43:30 +0200

    Add InventorySlotTextGUI component

---
+++ Assets/Scripts/InventorySlotTextGUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshPro))]
+public class InventorySlotTextGUI: MonoBehaviour
+{
+    private TextMeshPro _textMeshPro;
+
+    private void Awake()
+    {
+        _textMeshPro = GetComponent<TextMeshPro>();
+    }
+
+    public void SetText(string text)
+    {
+        _textMeshPro.text = text;
+    }
+}
